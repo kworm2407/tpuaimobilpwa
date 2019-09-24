099188decc01f5e4366b095b98f2fb20d5ac0367
kwormquery <nelson.boca.12@gmail.com>
2019-09-24 00:20:27 -0300

    Version 1.4

--- sw.js
+++ sw.js
@@ -1,34 +1,38 @@
 const cacheActual = 'UAIMobile-v2';
 
 const recursosEstaticos = [
-  'css/materialize.min.css',  
-  'js/materialize.min.js' , 
-  'icons/192.png'
+    'css/materialize.min.css',
+    'js/materialize.min.js',
+    'icons/192.png',
+    'img/Avatar_Aguirre_Nestor.jpg',
+    'img/Avatar_Aranda_Nelson.jpg',
+    'img/Avatar_Lomoro_Jorge.jpg',
+    'img/Avatar_Saavedra_Enzo.jpg',
+    'img/geofencing-icon-5.jpg',
+    'img/arrow-back-icon.png'
 ];
 
-self.addEventListener('install', function(event) 
-{
-  event.waitUntil
-  (
-    caches.open(cacheActual).then(function(cache) 
-       {
-        return cache.addAll(recursosEstaticos);
-       })
-  );
-  console.log('Installed ->', event);
-});	 
-	  
+self.addEventListener('install', function (event) {
+    event.waitUntil
+    (
+        caches.open(cacheActual).then(function (cache) {
+            return cache.addAll(recursosEstaticos);
+        })
+    );
+    console.log('Installed ->', event);
+});
+
 
 self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-  console.log('Fetch -> ', event);
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+    console.log('Fetch -> ', event);
 });
 
