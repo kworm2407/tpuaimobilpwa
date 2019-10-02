b4e1f26d44d2bd1538aa71c0f3855fc969f5372b
kwormquery <nelson.boca.12@gmail.com>
2019-10-02 01:22:03 -0300

    Mejora de la Interfaz Home V1.7

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
 var cacheActual = 'cachestore-v1.0';
-
+var CACHE_VERSION = 1;
+var CURRENT_CACHES = {
+    font: 'font-cache-v' + CACHE_VERSION
+};
 const recursosEstaticos = [
     'css/materialize.min.css',
     'js/materialize.min.js',
@@ -39,22 +42,57 @@ self.addEventListener('activate', function (event) {
     //             )
     //         )
     // );
+    // event.waitUntil(
+    //     caches.keys().then(cacheNames => Promise.all(
+    //         cacheNames.filter(cacheName => {
+    //             return cacheName !== cacheActual
+    //         }).map(cacheName => caches.delete(cacheName))
+    //     ))
+    // );
+    var expectedCacheNames = Object.values(CURRENT_CACHES);
     event.waitUntil(
-        caches.keys().then(cacheNames => Promise.all(
-            cacheNames.filter(cacheName => {
-                return cacheName !== cacheActual
-            }).map(cacheName => caches.delete(cacheName))
-        ))
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (!expectedCacheNames.includes(cacheName)) {
+                        console.log('Deleting out of date cache:', cacheName);
+
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
 
-self.addEventListener("fetch", function (evt) {
-    evt.respondWith(
-        caches.match(evt.request).then(function (response) {
-            return response || fetch(evt.request);
+self.addEventListener("fetch", function (event) {
+    console.log('Handling fetch event for', event.request.url);
+
+    event.respondWith(
+
+        // Opens Cache objects that start with 'font'.
+        caches.open(CURRENT_CACHES['font']).then(function(cache) {
+            return cache.match(event.request).then(function(response) {
+                if (response) {
+                    console.log('Found response in cache:', response);
+
+                    return response;
+                }
+
+                console.log('Fetching request from the network');
+
+                return fetch(event.request).then(function(networkResponse) {
+                    cache.put(event.request, networkResponse.clone());
+
+                    return networkResponse;
+                });
+            }).catch(function(error) {
+
+                // Handles exceptions that arise from match() or fetch().
+                console.error('Error in fetch handler:', error);
+
+                throw error;
+            });
         })
-        // fetch(event.request).catch(function () {
-        //     return caches.match(event.request);
-        // })
     );
 });
